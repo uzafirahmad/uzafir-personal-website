5c0fbcc4de7d2423d9d471c34da43d42a7e10f65
uzaifr ahmad <uzafir.a@panacealogics.com>
2023-12-12 10:12:54 +0500

    com

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <Navbar highlight="home"/>
-      <span className="pagetitle_home">Uzafir Ahmad.</span>
+      <span className="pagetitle_home">Uzafir Ahmad</span>
       <span className="secondarytitle_home">Full Stack Software Engineer</span>
       <div className="blurrygradient_2"/>
       <div className="blurrygradient_1"/>
